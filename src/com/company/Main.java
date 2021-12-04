5eaedd09642adf2cfd148899128ffe17c9234793
JonatanR <jonatanjr-2017@outlook.es>
2021-12-03 21:39:15 -0500

    Punto 'a' solucionado

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,8 +5,10 @@ public class Main
 
     public static void main(String[] args)
     {
-        // Declaracion y Instanciacion de Objetos Nodos p1,p2,p3,p4
+        System.out.println();
+        // Declaracion y Instanciacion de Objetos de la clase Nodo p1,p2,p3,p4
         Nodo p1, p2 , p3 , p4 , p5;
+        int auxiliar;
 
         p1 = new Nodo(7);
         p2 = new Nodo(4);
@@ -14,6 +16,14 @@ public class Main
         p4 = new Nodo(2);
         p5 = new Nodo(9);
 
+        System.out.println(" p1 : "+p1.GetNumero()+" p2: "+p2.GetNumero());
 
+        // a) Intercambiar de Info a los dos primeros
+
+        auxiliar = p1.GetNumero();
+        p1.SetNumero(p2.GetNumero());
+        p2.SetNumero(auxiliar);
+
+        System.out.println(" p1: "+p1.GetNumero()+" p2: "+p2.GetNumero());
     }
 }
