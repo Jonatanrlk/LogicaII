7db46a5b1dbd9efe816bad691343e7487e3d0a96
JonatanR <jonatanjr-2017@outlook.es>
2021-12-03 22:13:22 -0500

    Punto 'c' solucionado

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -43,5 +43,20 @@ public class Main
                 infoMayor = tercero;
             }
         }
+        System.out.println("El info mayor de los tres primeros es : "+infoMayor);
+
+        //Calcular El producto del Info de los Dos Ultimos(p4 y p5)
+
+        int cuarto , quinto , producto;
+
+        cuarto = p4.GetNumero();
+        quinto = p5.GetNumero();
+
+        producto = cuarto * quinto;
+        System.out.println("El producto de los Dos ultimos es : "+producto);
+
+
+
+
     }
 }
