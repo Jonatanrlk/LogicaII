6877c5ab38f1c1cebd9cb47ba9273b1865c550bf
Jonatan Romero <elfokincrack@gmail.com>
2022-01-03 11:27:52 -0500

    constructor listaligada

--- src/com/company/ListaLigada.java
+++ src/com/company/ListaLigada.java
@@ -2,6 +2,12 @@ package com.company;
 
 public class ListaLigada
 {
-    private Nodo cab , ult;
+    private Nodo cabeza , ultimo;
+
+    public ListaLigada()//constructor
+    {
+        cabeza = new Nodo(0);
+        ultimo = cabeza;
+    }
 
 }
