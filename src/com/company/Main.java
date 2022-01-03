adc14ac7de5a69c28e37ee0648afda581921634b
Jonatan Romero <elfokincrack@gmail.com>
2022-01-03 12:38:55 -0500

    subprograma Menu de Opciones

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,20 +1,44 @@
 package com.company;
 
+import java.util.*;
+
 public class Main
 {
     public static void main(String[] args)
     {
-        ListaLigada objeto = new ListaLigada();
+//        ListaLigada objeto = new ListaLigada();
+//
+//        int guardaNumero;
+//
+//
+//        guardaNumero = objeto.MostraLista();
+//
+//        while(guardaNumero != 0){
+//            System.out.println(guardaNumero+" ");
+//             guardaNumero= objeto.MostraLista();
+//        }
+//
+        Menu();
 
-        int guardaNumero;
+    }
 
+    public static char Menu()
+    {
+        Scanner input = new Scanner(System.in);
+        char opcion;
+        System.out.println("Menu De Opciones");
+        System.out.println("1.Mostrar Lista");
+        System.out.println("2.Ingresar un Nuevo Numero");
+        System.out.println("3.Eliminar el numero menor");
+        System.out.println("4.Insectar un numero Despues otro que entro como parametro");
+        System.out.println("5.Cuantos negativos hay");
+        System.out.println("6.Salir");
 
-        guardaNumero = objeto.MostraLista();
+        do {
+            System.out.println("Ingresar : ");
+            opcion = input.next().charAt(0);
+        }while(opcion < '1' || opcion > '6');
 
-        while(guardaNumero != 0){
-            System.out.println(guardaNumero+" ");
-             guardaNumero= objeto.MostraLista();
-        }
-
+        return opcion;
     }
 }
