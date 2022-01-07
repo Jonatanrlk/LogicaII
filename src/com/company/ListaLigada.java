b830abb74bb7ef7744cd0a2bfede3f6d49bd92a8
Jonatan Romero <elfokincrack@gmail.com>
2022-01-07 16:37:40 -0500

    atributos y metodo constructor

---
+++ src/com/company/ListaLigada.java
@@ -0,0 +1,28 @@
+package com.company;
+
+public class ListaLigada {
+
+    /*
+    la clase llamada LSL (lista simplemente
+    ligada), la cual tendrá dos datos privados de la clase nodoSimple, que llamaremos
+    primero y ultimo: primero apuntará hacia el primer nodo de la lista y ultimo apuntará hacia
+    el último nodo de la lista. Además, definiremos las operaciones que podremos efectuar
+    sobre objetos de dicha clase.
+     */
+
+    private Nodo primero ;
+    private Nodo ultimo ;
+
+    // Metodo constructor
+
+    public ListaLigada(){
+
+        primero = new Nodo(0);
+        ultimo = primero;
+    }
+
+
+
+
+
+}
